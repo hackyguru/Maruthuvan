470fbac22257a387080d66a4a2823112a538c2f8
npc203 <npc203@users.noreply.github.com>
2020-10-31 22:55:52 +0530

    added header

--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:Maruthuvan/utils/header.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFF0C9869),
+      ),
       body: Scaffold(
-          backgroundColor: Colors.greenAccent,
-          body: Center(child: Text("Yup Home here"))),
-      backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.greenAccent,
+        body: Column(children: [
+          HeaderWithSearchBox(
+            size: MediaQuery.of(context).size,
+            key: super.key,
+          ),
+        ]),
+      ),
     );
   }
 }
---
+++ lib/utils/header.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:Maruthuvan/utils/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      // It will cover 20% of our total height
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text('Welcome!',
+                    style: GoogleFonts.fredokaOne(
+                        fontSize: 30, color: Colors.white)),
+                Spacer(),
+                Image.asset("assets/transp-logo.png")
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Select your symptoms",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        // surffix isn't working properly  with SVG
+                        // thats why we use row
+                        // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
