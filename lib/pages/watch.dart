ec8027c26d90ebd20d8ef49921ddf436abe08899
Kumaraguru <19eucs071@skcet.ac.in>
2020-10-31 21:47:41 +0530

    first commit

---
+++ lib/pages/watch.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Watch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Watchie Watchie")),
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+}
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,11 +2,13 @@ import 'package:Maruthuvan/pages/chatbot.dart';
 import 'package:Maruthuvan/pages/detect.dart';
 import 'package:Maruthuvan/pages/explore.dart';
 import 'package:Maruthuvan/pages/homescreen.dart';
+import 'package:Maruthuvan/pages/watch.dart';
 import 'package:flutter/material.dart';
 
 final Map<Widget, Widget> pages = {
-  Icon(Icons.add, size: 30): HomeScreen(),
+  Icon(Icons.home, size: 30): HomeScreen(),
   Icon(Icons.list, size: 30): Explore(),
-  Icon(Icons.compare_arrows, size: 30): Detect(),
-  Icon(Icons.call_split, size: 30): Chatbot(),
+  Icon(Icons.center_focus_strong, size: 30): Detect(),
+  Icon(Icons.watch, size: 30): Watch(),
+  Icon(Icons.chat_bubble, size: 30): Chatbot(),
 };
