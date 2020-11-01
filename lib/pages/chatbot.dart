f6a1fcad56dab9ff8e1993aef77e4cd68615820c
Kumaraguru <19eucs071@skcet.ac.in>
2020-11-01 11:59:21 +0530

    adding watch v1

--- lib/pages/chatbot.dart
+++ lib/pages/chatbot.dart
@@ -3,9 +3,8 @@ import 'package:flutter/material.dart';
 class Chatbot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Chatty here"),
-      backgroundColor: Colors.greenAccent,
+    return Container(
+      
     );
   }
 }
